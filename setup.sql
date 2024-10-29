0eeeec06241465ee4ad71d0de19f2bbf0e59c728
HumanErr0r <82908215+HumanErr0r@users.noreply.github.com>
2024-10-29 17:35:36 -0400

    Created setup.sql
    this should be the starting set up to set up the data base

---
+++ setup.sql
@@ -0,0 +1,89 @@
+-- listing interest
+CREATE TABLE `listing_interest` (
+  `ListingInterestGroupID` int NOT NULL,
+  `ListingID` int NOT NULL,
+  `UserID` int NOT NULL,
+  PRIMARY KEY (`ListingInterestGroupID`),
+  UNIQUE KEY `ListingInterestGroupID_UNIQUE` (`ListingInterestGroupID`),
+  UNIQUE KEY `LisingID_UNIQUE` (`ListingID`),
+  UNIQUE KEY `UserID_UNIQUE` (`UserID`),
+  CONSTRAINT `ListingID` FOREIGN KEY (`ListingID`) REFERENCES `listings` (`ListingID`),
+  CONSTRAINT `ListingInterestUserID` FOREIGN KEY (`UserID`) REFERENCES `users` (`UserID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+
+-- listings
+CREATE TABLE `listings` (
+  `ListingID` int NOT NULL,
+  `OwnerID` int NOT NULL,
+  `PropertyID` int NOT NULL,
+  `SquareFeet` int DEFAULT NULL,
+  `Source` varchar(200) DEFAULT NULL,
+  `Price` int DEFAULT NULL,
+  `Rooms` int DEFAULT NULL,
+  `Title` varchar(45) DEFAULT NULL,
+  `Bathrooms` int DEFAULT NULL,
+  PRIMARY KEY (`ListingID`),
+  UNIQUE KEY `ListingID_UNIQUE` (`ListingID`),
+  UNIQUE KEY `OwnerID_UNIQUE` (`OwnerID`),
+  UNIQUE KEY `PropertyID_UNIQUE` (`PropertyID`),
+  CONSTRAINT `ListingsPropertyID` FOREIGN KEY (`PropertyID`) REFERENCES `property` (`PropertyID`),
+  CONSTRAINT `OwnerID` FOREIGN KEY (`OwnerID`) REFERENCES `users` (`UserID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+
+  -- preferences
+CREATE TABLE `preferences` (
+  `PreferenceID` int NOT NULL,
+  `UserID` int NOT NULL,
+  `ZipCode` varchar(45) DEFAULT NULL,
+  `Budget` int DEFAULT NULL,
+  `Rooms` int DEFAULT NULL,
+  `PropertyType` varchar(45) DEFAULT NULL,
+  `LeaseDuration` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`PreferenceID`),
+  UNIQUE KEY `PreferenceID_UNIQUE` (`PreferenceID`),
+  UNIQUE KEY `UserID_UNIQUE` (`UserID`),
+  CONSTRAINT `PreferencesUserID` FOREIGN KEY (`UserID`) REFERENCES `users` (`UserID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+
+-- property
+CREATE TABLE `property` (
+  `PropertyID` int NOT NULL,
+  `Street` varchar(45) DEFAULT NULL,
+  `City` varchar(45) DEFAULT NULL,
+  `State` varchar(45) DEFAULT NULL,
+  `ZipCode` varchar(45) DEFAULT NULL,
+  `SquareFeet` int DEFAULT NULL,
+  `Source` varchar(200) DEFAULT NULL,
+  PRIMARY KEY (`PropertyID`),
+  UNIQUE KEY `PropertyID_UNIQUE` (`PropertyID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+
+-- reviews
+CREATE TABLE `reviews` (
+  `ReviewID` int NOT NULL,
+  `PropertyID` int NOT NULL,
+  `UserID` int NOT NULL,
+  `Rating` int DEFAULT NULL,
+  `ReviewDate` datetime DEFAULT NULL,
+  `Description` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`ReviewID`),
+  UNIQUE KEY `ReviewID_UNIQUE` (`ReviewID`),
+  UNIQUE KEY `PropertyID_UNIQUE` (`PropertyID`),
+  UNIQUE KEY `UserID_UNIQUE` (`UserID`),
+  CONSTRAINT `PropertyID` FOREIGN KEY (`PropertyID`) REFERENCES `property` (`PropertyID`),
+  CONSTRAINT `PropertyUserID` FOREIGN KEY (`UserID`) REFERENCES `users` (`UserID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+
+-- users
+CREATE TABLE `users` (
+  `UserID` int NOT NULL,
+  `FirstName` varchar(45) DEFAULT NULL,
+  `LastName` varchar(45) DEFAULT NULL,
+  `Email` varchar(45) DEFAULT NULL,
+  `PhoneNumber` varchar(45) DEFAULT NULL,
+  `Password` varchar(45) DEFAULT NULL,
+  `Gender` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`UserID`),
+  UNIQUE KEY `UserID_UNIQUE` (`UserID`),
+  UNIQUE KEY `Email_UNIQUE` (`Email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
